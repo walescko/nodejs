35b61d83b40ef1e35a6afc2f1a2c906b686a0630
walescko <walescko@gmail.com>
2025-07-10 22:20:46 -0300

    test

--- RRL/C02/hello05.js
+++ RRL/C02/hello05.js
@@ -4,3 +4,5 @@ function sum(a,b){
 
 let soma = sum(2, 3)
 console.log(soma)
+console.log("Perdi a referência para usar um teclado full size")
+console.log("as teclas desse novo teclado são menores do que a do Redragon Fizz Pro")
