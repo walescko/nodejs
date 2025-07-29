7122a4cee95ce05f525e8d79c3743efe37b52b8b
walescko <walescko@gmail.com>
2025-07-29 19:48:40 -0300

    tcp

--- RRL/C04/hello-tcp.js
+++ RRL/C04/hello-tcp.js
@@ -1,9 +1,9 @@
 let net = require('net')
 let count = 1
 let server = net.createServer(function (socket) {
-    console.log('Cliente conectou do IP: ' + server.remoteAddress)
-    socket.edn('Hello World TCP:' + count++ + '\n')
+    console.log("Cliente conectou do IP: " + socket.remoteAddress)
+    socket.end("Hello World TCP: " + (count++) + "\n")
 })
 
-server.listen(3000, 'localhost')
+server.listen(3000, "localhost")
 console.log('Servidor TCP iniciado...')
